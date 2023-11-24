e1ceb92bb2325a89cd7d88528fd7dae1e6399ee7
raihanwebmaster <raihanemon2015@gmail.com>
2023-11-24 21:48:15 +0100

    remove some thing

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import { UserRoutes } from './app/modules/user/user.route';
-import { CustomError } from './app/CustomError';
 const app: Application = express();
 
 //parsers
@@ -15,11 +14,5 @@ app.get('/', (req: Request, res: Response) => {
     'Welcome to Programming Hero level 2 course Assignment 2 , Student: Raihan Uddin, Email: raihanemon2015@gmail.com  ',
   );
 });
-
-
-app.use((req: Request, res: Response,) => {
-  res.status(404).json({ error: 'Not Found' });
-});
-
 
 export default app;
